cfb8f168ed56b10f2a627fdb81f63be38e89cd36
Cathy <cathyludev@gmail.com>
2022-02-24 17:22:22 -0500

    Phase 3

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
 const {Pokemon, Type} = require('../db');
+const path = require('path');
 
 router.get('/', async(req, res, next) => {
-    res.send(await Pokemon.findAll());
+    try {
+        res.send(await Pokemon.findAll());
+    } catch(err) {
+        next(err)
+    }
+    
+});
+
+router.get('/pokemon', async(req, res, next) => {
+    try {
+        res.sendFile(path.join(__dirname, 'index.html'));
+    } catch(err) {
+        next(err)
+    }
 });
 
 module.exports = router;
